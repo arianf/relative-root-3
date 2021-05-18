f5e2ce5bfcfb2697973b4f7ede7d184939b836d6
Arian Faurtosh <arian@apple.com>
2021-05-17 22:33:35 -0700

    rails _3.2.22.5_ new relative-root-3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RelativeRoot3::Application.config.secret_token = '07e70658723c2aa53b1b29dbe968da3ac411df0071018e5d55cd986eb5f9d40a225fa567b6ae63798da3062c0339de1c2d974af069efc0adccd75f5827a15e40'
